683b71e96f9fa861d8820d134e3fcc510ce64844
Valera <yosh78@mail.ru>
2026-07-05 21:02:22 +0300

    Update seatd-helper.h

--- seatd-helper.h
+++ seatd-helper.h
@@ -1,19 +1,15 @@
-/* seatd-helper.h - интеграция turnstile-dbus с seatd */
+/* seatd-helper.h - turnstile-dbus & seatd */
 #ifndef SEATD_HELPER_H
 #define SEATD_HELPER_H
 
 #include <libseat.h>
 
-/* Инициализация подключения к seatd */
 int seatd_helper_init(void);
 
-/* Получение DRM fd для сессии */
 int seatd_helper_get_drm_fd(unsigned long session_id);
 
-/* Получение input fd для сессии */
 int seatd_helper_get_input_fds(int **fds, int *count, unsigned long session_id);
 
-/* Закрытие соединения */
 void seatd_helper_cleanup(void);
 
 #endif
